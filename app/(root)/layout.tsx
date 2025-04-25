5376d5e52ce5b68f7f1f0aa26a6524825392cd56
Keyur Chaudhari <keyurchaudhari4@gmail.com>
2025-04-25 19:48:55 +0530

    Code cleanup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import { PropsWithChildren } from 'react';
+import Navbar from '../components/Navbar';
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <main className='font-work-sans'>
+      <Navbar />
+      {children}
+    </main>
+  );
+};
+
+export default Layout;
