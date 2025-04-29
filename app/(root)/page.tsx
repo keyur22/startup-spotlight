713d1213213eb813b885164df398f09ac5e47eee
Keyur Chaudhari <keyurchaudhari4@gmail.com>
2025-04-29 13:56:51 +0530

    Added shadcn, Added Hero section on Homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <p className='text-2xl'>Home</p>;
-}
+const HomePage = () => {
+  return (
+    <>
+      <section className='pink_container'>
+        <h1 className='heading'>
+          Pitch Your Startup, <br /> Connect With Enterpreneurs
+        </h1>
+        <p className='sub-heading !max-w-3xl'>
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+      </section>
+    </>
+  );
+};
+
+export default HomePage;
