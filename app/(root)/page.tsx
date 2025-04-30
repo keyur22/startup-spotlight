2501f76222a2a0279d0e85dc676da217a318a724
Keyur Chaudhari <keyurchaudhari4@gmail.com>
2025-04-30 16:10:29 +0530

    Added sanity live for auto update of list on homepage without page reload, using cache revalidation strategy for next.js

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import SearchForm from '@/components/SearchForm';
 import StartupCard, { StartupTypeCard } from '@/components/StartupCard';
-import { client } from '@/sanity/lib/client';
+
+import { sanityFetch, SanityLive } from '@/sanity/lib/live';
 import { STARTUPS_QUERY } from '@/sanity/lib/queries';
 
 const HomePage = async ({
@@ -9,7 +10,8 @@ const HomePage = async ({
   searchParams: Promise<{ query: string }>;
 }) => {
   const { query } = await searchParams;
-  const posts = await client.fetch(STARTUPS_QUERY);
+
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
   return (
     <>
@@ -37,6 +39,7 @@ const HomePage = async ({
           )}
         </ul>
       </section>
+      <SanityLive />
     </>
   );
 };
