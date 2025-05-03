2894792e8f533d9bc89c0f41acd2efeed2f84bd6
Keyur Chaudhari <keyurchaudhari4@gmail.com>
2025-05-03 18:51:57 +0530

    Added revalidate flag for homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,8 @@
 import SearchForm from '@/components/SearchForm';
 import StartupCard, { StartupTypeCard } from '@/components/StartupCard';
 
+export const revalidate = 5;
+
 import { sanityFetch, SanityLive } from '@/sanity/lib/live';
 import { STARTUPS_QUERY } from '@/sanity/lib/queries';
 
